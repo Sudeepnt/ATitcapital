de1f37fc3f6d6a53496f6ee8e86e9aabff1ab7a5
Sudeep N T <sudeepnt01@gmail.com>
2026-02-01 18:11:30 +0530

    Fix build error: Update business-view page to pass service object instead of slug

--- app/business-view/[slug]/page.tsx
+++ app/business-view/[slug]/page.tsx
@@ -1,6 +1,17 @@
 import BusinessSideView from "../../components/business-side-view";
+import { getCMSData } from "../../actions/cmsActions";
+import { slugify } from "../../utils/slugify";
+import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
-    return <BusinessSideView slug={slug} />;
+    const data = await getCMSData();
+
+    const service = data?.services?.items?.find((item: any) => slugify(item.title) === slug);
+
+    if (!service) {
+        redirect("/services");
+    }
+
+    return <BusinessSideView service={service} />;
 }
